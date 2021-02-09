065ec3f8a5923ec998742d01b0c224f3074b11ac
kauwai <53876126+kauwai@users.noreply.github.com>
2021-02-09 18:00:04 -0300

    Change current user to agree with db logic

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Rank({ currentUser, ranking }) {
-  const { name } = currentUser;
+  const { name } = currentUser[0];
   return (
     <div>
       <div className="white f3">{`${name}, your current rank is...`}</div>
